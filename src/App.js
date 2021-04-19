9ce8f1c5163abe85ed0beec5220a734040c20c03
thang241190 <thangnguyen24111990@gmail.com>
2021-04-19 23:19:07 +0300

    Finish ex4.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import { Container, Grid, Paper, Typography, Box } from "@material-ui/core"
+import getz from "./getz.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		margin: theme.spacing(2),
+		padding: theme.spacing(2, 1),
+		textAlign: "center",
+		color: theme.palette.text.secondary,
+		display: "flex",
+	},
+	text: {
+		border: "1px solid #333",
+		padding: "0 10px",
+	},
+}))
+
+export default function App() {
+	const classes = useStyles()
+	const [text, setText] = useState("")
+	const [image, setImage] = useState(getz)
+	const pagePaste = (event) => {
+		var item = event.clipboardData.items[0]
+
+		if (item.type.indexOf("image") === 0) {
+			var blob = item.getAsFile()
+
+			var reader = new FileReader()
+			reader.onload = function (event) {
+				document.getElementById("container").src = event.target.result
+			}
+
+			reader.readAsDataURL(blob)
+		}
+	}
+
+	return (
+		<div
+			className={classes.root}
+			contentEditable={true}
+			onPaste={pagePaste}>
+			<Container maxWidth='sm'>
+				<Grid container>
+					<Paper elevation={2} className={classes.paper}>
+						<Typography className={classes.text} variant='h6'>
+							Hmm kimochi
+						</Typography>
+						<Box>
+							<img
+								src={image}
+								alt='Old Huyndai Getz'
+								id='DraggingImage'
+							/>
+						</Box>
+					</Paper>
+				</Grid>
+			</Container>
+		</div>
+	)
 }
-
-export default App;
